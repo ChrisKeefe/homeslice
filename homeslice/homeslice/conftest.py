9ee55942e122fc22a921080e5602492d1f4710da
Chris Keefe <crk239@nau.edu>
2021-06-05 14:50:34 -0700

    Initial config with cookiecutter

---
+++ homeslice/homeslice/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homeslice.users.models import User
+from homeslice.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
